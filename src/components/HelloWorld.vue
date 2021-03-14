ac07a590d60adeae53b1cd871f4a36f2405fe879
Matt <mpau548@aucklanduni.ac.nz>
2021-03-15 01:09:36 +1300

    Initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+
+    <h1>{{ msg }}</h1>
+
+    <p>
+      <a
+        href="https://vitejs.dev/guide/features.html"
+        target="_blank"
+      >Vite Documentation</a> |
+      <a
+        href="https://vuejs.org/v2/guide/"
+        target="_blank"
+      >Vue 2 Documentation</a>
+    </p>
+
+    <button @click="state.count++">count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
